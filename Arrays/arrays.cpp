5a744556bd03ec8a987115abd5126b97d10c5046
Ujjwal Jaguri <ujjwal.jaguri.19012003@gmail.com>
2024-04-29 08:23:10 +0530

    Did some 2d arrays basic questions

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -136,16 +136,91 @@ void shiftElementBy2(int arr[], int size)
     cout << ")";
 }
 
+void sumOfDiagElem(int arr[][4], int rows, int cols)
+{
+    cout << "Size of row is: " << rows << "\n";
+    cout << "Size of col is: " << cols << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int sumOfDiag = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = cols - 1; j >= 0; j--)
+        {
+            if (i + j == rows - 1)
+            {
+                sumOfDiag += arr[j][i];
+            }
+        }
+    }
+    cout << "Sum of diagonal is: " << sumOfDiag;
+}
+
+void transposeOfMatrix(int arr[][4], int rows, int cols)
+{
+    cout << "Before transposing the matrix" << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = i; j < cols; j++)
+        {
+            swap(arr[i][j], arr[j][i]);
+        }
+    }
+
+    cout << "After transposing the matrix" << endl;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 void printArrays()
 {
     cout << "Hi there! I am from arrays.cpp" << endl;
-    int arr[6] = {10, 20, 30, 40, 50, 60};
-    int size = 6;
 
+    // ----------->>> FINDING UNIQUE ELEMENT IN THE ARRAY <<<<------------------
+
+    // int arr[6] = {10, 20, 30, 40, 50, 60};
+    // int size = 6;
     // int uniqueElement = findUniqueElement(arr, size);
     // cout << uniqueElement;
     // printPairs(arr, size);
     // sortZeroAndOne(arr, size);
     // shiftElementBy1(arr, size);
     // shiftElementBy2(arr, size);
+
+    // ----------->>> FINDING SUM OF DIAGONAL ELEMENTS <<<<------------------
+
+    // int arr[4][4] = {
+    //     {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    // int rows = sizeof(arr) / sizeof(arr[0]);
+    // int cols = sizeof(arr[0]) / sizeof(int);
+    // sumOfDiagElem(arr, rows, cols);
+
+    // ----------->>> TRANSPOSING A MATRIX (2D ARRAY) <<<<------------------
+    // int arr[4][4] = {{1, 2, 3, 4}, {4, 5, 6, 7}, {7, 8, 9, 0}, {3, 5, 6, 6}};
+
+    // int rows = sizeof(arr) / sizeof(arr[0]);
+    // int cols = sizeof(arr[0]) / sizeof(int);
+    // transposeOfMatrix(arr, rows, cols);
 }
